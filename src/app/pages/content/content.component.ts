6a5b41d5d8408b25c954623219b6a8f59857147e
Juan pablo mendes <jp436807@gmail.com>
2024-02-14 18:20:42 -0300

    arrumando responsividade

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -5,7 +5,7 @@ import {dataFake} from '../../data/dataFake';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css','content.component-response.css']
 })
 export class ContentComponent implements OnInit {
   @Input()
